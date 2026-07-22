312f292b2bcc400c0546185e4af8ff464e0b16b8
lavrentev.makar <makar.lavrentev@yandex.ru>
2026-07-22 14:41:57 +0300

    Added structs for metric models

--- internal/model/models.go
+++ internal/model/models.go
@@ -1,1 +1,16 @@
 package model
+
+import (
+	"time"
+)
+
+type ServerMetrics struct {
+	Name string
+	CheckTime time.Time
+	Metrics []Metric
+}
+
+type Metric struct {
+	Name string
+	Value float32
+}
